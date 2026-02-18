b0628a5ae7f6d1299dbe39e24abd9035bcbc0ce2
kiranP <keyrun602@gmail.com>
2026-02-18 13:13:42 +0545

    add edit button with code correction.

--- src/components/SingleItem.jsx
+++ src/components/SingleItem.jsx
@@ -1,10 +1,15 @@
 import { FiEdit, FiTrash2 } from "react-icons/fi";
 import "./SingleItem.css";
 
-const SingleItem = ({ item }) => {
+const SingleItem = ({ item, editCompleted }) => {
   return (
     <div className="single-item">
-      <input type="checkbox" checked={item.completed} onChange={() => null} />
+      <input
+        type="checkbox"
+        checked={item.completed}
+        onChange={() => editCompleted(item.id)}
+      />
+
       <p
         style={{
           textTransform: "capitalize",
@@ -14,14 +19,18 @@ const SingleItem = ({ item }) => {
         {item.name}
       </p>
 
-      <button className="btn icon-btn" type="button" onClick={() => null}>
+      <button
+        className="btn icon-btn"
+        type="button"
+        onClick={() => editCompleted(item.id)}
+      >
         <FiEdit size={18} />
       </button>
 
       <button
         className="btn icon-btn remove-btn"
         type="button"
-        onClick={() => null}
+        onClick={() => editCompleted(item.id)}
       >
         <FiTrash2 size={18} />
       </button>
